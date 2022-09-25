2870ad39b1facfa9a66c71b95bb0024a0ac38b71
Lauren Black <laurenblack5280@gmail.com>
2022-09-24 19:52:55 -0600

    add functions for display, add promise all

--- src/scripts.js
+++ src/scripts.js
@@ -8,7 +8,6 @@ console.log('This is the JavaScript entry file - your code begins here.');
 //imports
 import TravelerRepo from './TravelerRepo'
 import Traveler from './Traveler'
-
 import {getAllTravelers, getAllTrips, getAllDestinations} from './apiCalls'
 
 //global variables
@@ -23,3 +22,45 @@ const pastTrips = document.getElementById('')
 const upcomingTrips = document.getElementById('')
 const pendingTrips = document.getElementById('')
 const totalSpent = = document.getElementById('')
+
+//event listeners
+window.addEventListener('load', getAllData())
+
+//promise
+function getAllData() {
+  Promise.all([getAllTravelers, getAllTrips, getAllDestinations]).then((data) => {
+    allTravelers = data[0].allTravelers
+    allTrips = data[1].allTrips
+    allDestinations = data[2].allDestinations
+    travelerRepo = new TravelerRepo()
+    traveler = new Traveler(allTravelers[Math.floor(Math.random() * allTravelers.length)])
+    populateDashboard()
+  })
+}
+
+//functions
+/*
+function populateDashboard() {
+displayPastTrips()
+displayUpcomingTrips()
+displayPendingTrips()
+displayTotalSpent()
+}
+
+function displayPastTrips() {
+
+}
+
+function displayUpcomingTrips() {
+
+}
+
+function displayPendingTrips() {
+
+}
+
+function displayTotalSpent() {
+
+}
+
+*/
